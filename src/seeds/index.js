3c15f859df9d8fdc42f86d7d389bf66ba4ecda99
ok-qa <olechka.kr@gmail.com>
2025-01-16 11:30:39 -0800

    Updated index files

--- src/seeds/index.js
+++ src/seeds/index.js
@@ -9,15 +9,15 @@ import runRecipesSeed from "./recipes.js";
 import { runTwoCategoriesRecipeSeed } from "./testRecipe.js";
 
 const runAllSeeds = async () => {
-  // await RecipesCollection.deleteMany({});
-  // await AreasCollection.deleteMany({});
-  // await CategoriesCollection.deleteMany({});
-  // await IngredientsCollection.deleteMany({});
+  await RecipesCollection.deleteMany({});
+  await AreasCollection.deleteMany({});
+  await CategoriesCollection.deleteMany({});
+  await IngredientsCollection.deleteMany({});
 
-  // await runAreasSeed();
-  // await runCategoriesSeed();
-  // await runIngredientsSeed();
-  // await runRecipesSeed();
+  await runAreasSeed();
+  await runCategoriesSeed();
+  await runIngredientsSeed();
+  await runRecipesSeed();
   await runTwoCategoriesRecipeSeed();
 };
 
